3570de7d0c56f846c753f09e60300e91eaa0a2d8
haris0 <hari.surrisyad@gmail.com>
2019-08-29 17:46:11 +0700

    install vue matrial, and design for homepage

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,11 +1,69 @@
 <template>
-  <div>
-      
+  <div class="container">
+    <div class="search">
+      <div class="search-title">
+        <span>
+          Github Search
+        </span>
+      </div>      
+      <md-field>
+        <label>Github id</label>
+        <md-input v-model="id"></md-input>
+        <md-icon>search</md-icon>
+      </md-field>
+      <div class="search-button">
+        <md-button @click="goToProfile" class="md-dense md-raised md-primary button-search">Search</md-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+
+  let self;
+
   export default {
-      
+    name: 'Homepage',
+    created(){
+      self = this;  
+    },
+    mounted(){
+    },
+    data() {
+      return {
+        id:""
+      }
+    },
+    methods:{
+      goToProfile: function(){
+        console.log(self.id)
+      }
+    }
   }
+
 </script>
+
+<style scoped>
+  .container{
+    height:100vh;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+  }
+  .search{
+    width: 75%;
+  }
+  .search-title{
+    margin: 20px;
+    text-align: center;
+    font-size: 30px;
+    font-family: monospace;
+    color: darkgray;
+  }
+  .search-button{
+    text-align: center !important;
+  }
+  .button-search{
+    background-color: gray !important;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Homepage from '@/components/Homepage'
+import { MdButton, MdField} from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
+Vue.use(MdButton)
+Vue.use(MdField)
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {
