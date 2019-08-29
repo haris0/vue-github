6fc56b1cc30761faaf8167e9e6d1f403d0b083f4
haris0 <hari.surrisyad@gmail.com>
2019-08-29 23:44:58 +0700

    handle not font account

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -5,52 +5,54 @@
     </md-toolbar>
     <div class="container">
 
-      <div v-if="profile == null" class="center" >
+      <div v-if="isLoading" class="center" >
         <md-progress-spinner md-mode="indeterminate"></md-progress-spinner>
       </div>
 
       <div v-else>
-        <md-subheader>Profile</md-subheader>
-        <md-card>
-          <md-card-header>
-            <md-avatar>
-              <img :src="profile.avatar_url" alt="Avatar">
-            </md-avatar>
-            <div class="md-title">{{profile.name}}</div>
-            <div class="md-subhead">{{profile.login}}</div>
-          </md-card-header>
-          <md-card-content>
-            {{profile.bio}}
-          </md-card-content>
-        </md-card>
-        
-        <md-subheader>Project</md-subheader>
-        <div v-for="repo in repos" v-bind:key="repo.full_name" class="list-project">
-          <md-card md-with-hover>
-            <md-ripple>
-              <md-card-header>
-                <div class="md-title">{{repo.name}}</div>
-                <div class="md-subhead">{{repo.language}}</div>
-              </md-card-header>
-              <md-card-content>
-                {{repo.description}}
-              </md-card-content>
-              <md-card-actions>
-                <md-button>Open</md-button>
-              </md-card-actions>
-            </md-ripple>
+        <div v-if="profile == null">
+          <md-empty-state
+            md-icon="highlight_off"
+            md-label="Not Font"
+            md-description="The github account you are looking for was not found, please try again">
+            <md-button class="md-primary md-raised">Back to Home</md-button>
+          </md-empty-state>
+        </div>
+        <div v-else>
+          <md-subheader>Profile</md-subheader>
+          <md-card>
+            <md-card-header>
+              <md-avatar>
+                <img :src="profile.avatar_url" alt="Avatar">
+              </md-avatar>
+              <div class="md-title">{{profile.name}}</div>
+              <div class="md-subhead">{{profile.login}}</div>
+            </md-card-header>
+            <md-card-content>
+              {{profile.bio}}
+            </md-card-content>
           </md-card>
+          
+          <md-subheader>Project</md-subheader>
+          <div v-for="repo in repos" v-bind:key="repo.full_name" class="list-project">
+            <md-card md-with-hover>
+              <md-ripple>
+                <md-card-header>
+                  <div class="md-title">{{repo.name}}</div>
+                  <div class="md-subhead">{{repo.language}}</div>
+                </md-card-header>
+                <md-card-content>
+                  {{repo.description}}
+                </md-card-content>
+                <md-card-actions>
+                  <md-button>Open</md-button>
+                </md-card-actions>
+              </md-ripple>
+            </md-card>
+          </div>
         </div>
       </div>
     </div>
-    <!-- <div>
-      <md-empty-state
-        md-icon="highlight_off"
-        md-label="Not Font"
-        md-description="The github account you are looking for was not found, please try again">
-        <md-button class="md-primary md-raised">Back to Home</md-button>
-      </md-empty-state>
-    </div> -->
   </div>
 </template>
 
@@ -73,18 +75,21 @@
       return {
         id: "",
         profile: null,
-        repos: []
+        repos: [],
+        isLoading: true
       }
     },
     methods:{
       getProfile : async() => {
         try{
           let response = await config.HTTP.get(config.getProfile+self.id)
+          self.isLoading = false;
           console.log(self.profile)
           self.profile = response.data;
           console.log(self.profile);
           self.getRepos();
         }catch(error){
+          self.isLoading = false;
           console.log(error.response);
         }
       },
